118943c0199a702fc6b6334957bf308380e61cbf
Luis Cortes <cortes72luis@gmail.com>
2023-08-05 12:42:01 -0700

    feat: Add statement cloning
    Updated packages
    Added /statements/:statementId/clone route

--- src/components/admin/Statements.jsx
+++ src/components/admin/Statements.jsx
@@ -1,4 +1,16 @@
-import { Alert, Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import {
+    Alert,
+    Box,
+    Button, IconButton, List, ListItem, ListItemButton, ListItemText, Paper, Popper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useApi } from '../../providers/ApiProvider';
@@ -6,12 +18,16 @@ import months from '../../util/months';
 import messageService from '../../core/services/messageService';
 
 const Statements = () => {
-
     const [ statements, setStatements ] = useState([]);
     const [ successMsg, setSuccessMsg ] = useState(messageService.get());
+    const [ anchorEl, setAnchorEl ] = useState();
+    const [ selectedPhone, setSelectedPhone ] = useState({ });
 
     const navigate = useNavigate();
     const api = useApi();
+
+    const menuOpen = Boolean(anchorEl);
+    const id = open ? 'simple-popper' : undefined;
 
     useEffect(() => {
         (async () => {
@@ -23,10 +39,15 @@ const Statements = () => {
         })();
     }, [ api ]);
 
+    const handleMenuClick = (event, month, year) => {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+        setSelectedPhone({ month, year });
+        
+    };
     const onStatementClick = (month, year) => navigate(`${ year }-${ month }`);
 
     return (
-        <>
+        <div>
             <Box mb={3}>
                 {
                     successMsg && 
@@ -51,7 +72,7 @@ const Statements = () => {
                     Create Statement
                 </Button>
             </Box>
-
+            
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
@@ -59,6 +80,7 @@ const Statements = () => {
                             <TableCell>Year</TableCell>
                             <TableCell>Month</TableCell>
                             <TableCell>Create  Date</TableCell>
+                            <TableCell colSpan={1}></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -75,6 +97,38 @@ const Statements = () => {
                                 <TableCell>{year}</TableCell>
                                 <TableCell>{months[month]}</TableCell>
                                 <TableCell>{new Date(createDateTime).toLocaleDateString('en-US')}</TableCell>
+                                <TableCell 
+                                    colSpan={1} 
+                                    align='right'>
+                                    <IconButton
+                                        aria-describedby={id}
+                                        sx={{ '&:hover': { backgroundColor: 'unset', opacity: 1 }, }}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleMenuClick(e, month, year);
+                                        }}
+                                    >
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                    <Popper open={menuOpen} anchorEl={anchorEl} placement='bottom-end'>
+                                        <Paper>
+                                            <List>
+                                                <ListItem disablePadding>
+                                                    <ListItemButton>
+                                                        <ListItemText
+                                                            primary="Clone"
+                                                            onClick={(e) => {
+                                                                e.stopPropagation();
+
+                                                                navigate(`${ selectedPhone.year }-${ selectedPhone.month }/clone`);
+                                                            }}
+                                                        />
+                                                    </ListItemButton>
+                                                </ListItem>
+                                            </List>
+                                        </Paper>
+                                    </Popper>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -82,7 +136,7 @@ const Statements = () => {
             </TableContainer>
 
             <Outlet />
-        </> 
+        </div>
     );
 };
 
